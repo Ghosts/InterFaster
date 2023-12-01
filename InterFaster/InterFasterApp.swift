bbe100e476445edef87fd8f25670fedd82b57a56
Caden Sumner <cadenjsumner@gmail.com>
2023-12-01 11:54:25 -0700

    Initial Commit

---
+++ InterFaster/InterFasterApp.swift
@@ -0,0 +1,32 @@
+//
+//  InterFasterApp.swift
+//  InterFaster
+//
+//  Created by Caden Sumner on 12/1/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InterFasterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
